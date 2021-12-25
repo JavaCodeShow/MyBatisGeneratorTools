ba92d456fd998e81054f9c6e30bb6463d51ff2d9
江峰 <feng.jiang@marketin.cn>
2021-12-25 19:01:34 +0800

    fix

--- src/main/java/com/tjcu/Tools/GeneratorSqlmap.java
+++ src/main/java/com/tjcu/Tools/GeneratorSqlmap.java
@@ -24,7 +24,7 @@ public class GeneratorSqlmap {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         //指定 逆向工程配置文件
-        File configFile=new File(GeneratorSqlmap.class.getResource("/generatorConfig.xml").getFile());
+        File configFile = new File(GeneratorSqlmap.class.getResource("/generatorConfig.xml").getFile());
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
@@ -34,7 +34,7 @@ public class GeneratorSqlmap {
 
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
             GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
             generatorSqlmap.generator();
