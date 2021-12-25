e24e994427bbdef932bb3191da48d331ac323619
江峰 <feng.jiang@marketin.cn>
2021-12-25 19:12:53 +0800

    fix

---
+++ src/main/java/com/tjcu/entity/Account.java
@@ -0,0 +1,35 @@
+package com.tjcu.entity;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private Integer id;
+
+    private BigDecimal money;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
